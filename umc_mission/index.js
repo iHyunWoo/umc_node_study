bf7060207a97c28e12020d3a1158944dba120368
HyunWoo Jeong <jrwedo@naver.com>
2024-05-27 13:53:00 +0900

    [FEAT] temp 기능과 ErrorHandler 적용

--- umc_mission/index.js
+++ umc_mission/index.js
@@ -1,13 +1,33 @@
-// const express = require('express')   // common JS
 import express from 'express'          // ES6
+import { tempRouter } from './src/routes/temp.route.js';
+import { response } from './src/config/response.js';
+import { BaseError } from './src/config/error.js';
+import { status } from './src/config/response.status.js';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+// router setting
+app.use('/temp', tempRouter);
+
+// error handling
+
+app.use((req, res, next) => {
+    const err = new BaseError(status.NOT_FOUND);
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    // 템플릿 엔진 변수 설정
+    res.locals.message = err.message;   
+    // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
+    res.status(err.data.status).send(response(err.data));
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
+
