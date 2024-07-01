618491c18cfb5ff609254b30a81c8f62eca1e72b
HyunWoo Jeong <100191598+iHyunWoo@users.noreply.github.com>
2024-07-01 12:56:47 +0900

    Revert "[FEAT] store API 페이징 구현"

--- src/routes/review.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import asyncHandler from "express-async-handler";
-import reviewController from "../controllers/review.controller.js";
-
-export const reviewRouter = express.Router();
-
-reviewRouter.get('/:memberId', asyncHandler(reviewController.getReviews))
-reviewRouter.get('/store/:storeId', asyncHandler(reviewController.getReviewsWithStoreId))
